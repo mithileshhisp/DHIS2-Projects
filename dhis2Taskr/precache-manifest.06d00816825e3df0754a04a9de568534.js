775daf6ce47b6bda3466e20109e990b71e5dd1a1
Mithilesh Kumar Thakur <mithilesh.hisp@gmail.com>
2020-07-23 11:04:02 +0530

    add dhis2Taskr build

---
+++ dhis2Taskr/precache-manifest.06d00816825e3df0754a04a9de568534.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9323f329cfdfa024bd94780f43b0957c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5ddeb2c1659ca99faa54",
+    "url": "./static/css/2.8e175a64.chunk.css"
+  },
+  {
+    "revision": "f595b9d5b2757c1eccee",
+    "url": "./static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "5ddeb2c1659ca99faa54",
+    "url": "./static/js/2.265a7a9e.chunk.js"
+  },
+  {
+    "revision": "f595b9d5b2757c1eccee",
+    "url": "./static/js/main.80dcd1bf.chunk.js"
+  },
+  {
+    "revision": "d3994998600983bab00d",
+    "url": "./static/js/runtime-main.ec63e1d5.js"
+  }
+]);
